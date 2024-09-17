c2e22a4a075580d3dbf2af007e97a5dc7a50c297
Pallavi Chandaka <163713669+Pallavichandaka@users.noreply.github.com>
2024-09-17 18:43:26 +0530

    SubsequenceWithSumK.cpp

--- RECURSION/SubsequenceWithSumK.cpp
+++ RECURSION/SubsequenceWithSumK.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 class Solution {
-    bool func(int ind, int sum, const vector<int>& nums) {
-      //base-condition  
-        if (sum == 0) return true;
-        if (sum < 0 || ind == nums.size()) return false;
-      
-        bool path1 = func(ind + 1, sum - nums[ind], nums);
-        bool path2 = func(ind + 1, sum, nums);
+    bool func(int ind, int currSum, const vector<int>& nums, int k) {
+        if (currSum == k) return true;
+        if (currSum > k || ind == nums.size()) return false;
+        
+        bool path1 = func(ind + 1, currSum + nums[ind], nums, k);
+        bool path2 = func(ind + 1, currSum, nums, k);
+        
         return path1 || path2;
     }
 
+public:
     bool checkSubsequenceSum(vector<int>& nums, int k) {
-        return func(0, k, nums);
+        return func(0, 0, nums, k);
     }
 };
 
@@ -23,7 +24,8 @@ int main() {
     
     vector<int> nums = {1, 2, 3, 4, 5};
     int k = 8;
-    cout << (sol.checkSubsequenceSum(nums, k) ? "Yes" : "No") << endl;  // Expected output: Yes
+    cout << (sol.checkSubsequenceSum(nums, k) ? "Yes" : "No") << endl;
 
     return 0;
 }
+
